d280bc2d3c349b38e3307139b5e3b707198018f7
undineleopold <79616083+undineleopold@users.noreply.github.com>
2022-06-28 15:58:40 -0400

    Update cities_app.py

--- flaskapp_cities/cities_app.py
+++ flaskapp_cities/cities_app.py
@@ -6,18 +6,14 @@ from markupsafe import Markup
 app = Flask(__name__)
 #app.config['TEMPLATES_AUTO_RELOAD']=True
 
+
+'''This should all eventually go into a app_db.py file---'''
+
 #function connecting to the database
 def get_db_con():
     con=sqlite3.connect('cities.db')
     con.row_factory=sqlite3.Row #is that the same as a cursor?
     return con
-
-#function querying the database on city data and a partial string
-def query_db(con, city: str, partial:str ):
-    #make sure input is sanitized
-    sql='''SELECT * FROM waste_table WHERE city=? AND material LIKE ?'''
-    args=(city,"%"+partial+"%")#should be safe according to https://bobby-tables.com/python, I think
-    return con.execute(sql,args).fetchone()
 
 #function querying the database on all materials in the city's list
 def matlist_db(con, city:str):
@@ -25,6 +21,21 @@ def matlist_db(con, city:str):
     args=(city,)
     return con.execute(sql,args).fetchall()
 
+#function querying the database on city data and a partial string
+def query_db(con, city: str, partial:str ):
+    #make sure input is sanitized
+    sql='''SELECT * FROM waste_table WHERE city=? AND material LIKE ?'''
+    args=(city,"%"+partial+"%")#should be safe according to https://bobby-tables.com/python, I think
+    answer=con.execute(sql,args).fetchone()
+    if answer is not None:
+       result={'material':answer['material'], 'category':answer['category'], 'instructions':Markup(answer['instructions']).unescape()}
+    else:
+       result=None
+    return result
+
+'''------------------------------------------------------'''
+
+'''This is the frontend part ----------------------------'''
 #index page functionality
 @app.route('/', methods=['GET'])
 def index():
@@ -50,13 +61,9 @@ def handle_data():
 #handle wizard inputs
 @app.route('/search/<city>', methods=['POST']) #add GET method to allow direct access via browser
 def search(city):
-   con=get_db_con() #when does this close? can the connection be reused
+   con=get_db_con() #when does this close? can the connection be reused, look into flask g object
    citylist=matlist_db(con,city)
-   answer=query_db(con,city,escape(request.form['search']))
-   if answer is not None:
-       result={'material':answer['material'], 'category':answer['category'], 'instructions':Markup(answer['instructions']).unescape()}
-   else:
-       result=None
+   result=query_db(con,city,escape(request.form['search']))
    return render_template('wizard.html', city=city, citylist=citylist, result=result)
 
 app.run(debug=True)
