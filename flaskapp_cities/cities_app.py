fc79a0aa48b7e9a400f6e078d6da967830253bb9
undineleopold <79616083+undineleopold@users.noreply.github.com>
2022-07-01 22:12:43 -0400

    updated DB with another city, introduced city_id

--- flaskapp_cities/cities_app.py
+++ flaskapp_cities/cities_app.py
@@ -26,15 +26,29 @@ def close_db(e): #why does this function need an argument?
     if db is not None:
         db.close() #close the database connection
 
-#function querying the database on all materials in the city's list
-def matlist_db(city:str):
+#function querying the database on cities present
+def get_cities_db():
     con=get_db_con()
-    sql='''SELECT material FROM waste_table WHERE city=?'''
-    args=(city,)
+    sql='''SELECT * FROM city_table'''
+    return con.execute(sql).fetchall()
+
+#function querying the database on city data by id
+#assume city_id is a unique identifier in the table
+def get_city_db(city_id):
+    con=get_db_con()
+    sql='''SELECT * FROM city_table WHERE city_id=?'''
+    args=(city_id,)
+    return con.execute(sql,args).fetchone()
+
+#function querying the database on all materials in the city's list
+def matlist_db(city_id:str):
+    con=get_db_con()
+    sql='''SELECT material FROM waste_table WHERE city_id=?'''
+    args=(city_id,)
     return con.execute(sql,args).fetchall()
 
 #function querying the database on city data and a partial string
-def query_db(city: str, partial:str):
+def query_db(city_id: str, partial:str):
     con=get_db_con() #typically, we call matlist_db() first, so the connection
                      #is already open
     #partial should be escaped already
@@ -43,12 +57,12 @@ def query_db(city: str, partial:str):
     else:
         if partial[-1]==u"\u2063": #user selected from suggestions with invisible separator
             #exact match (case insensitive) after removing invisible character
-            sql='''SELECT * FROM waste_table WHERE city=? AND material LIKE ?'''
-            args=(city, partial[:-1])#should be safe according to https://bobby-tables.com/python, I think
+            sql='''SELECT * FROM waste_table WHERE city_id=? AND material LIKE ?'''
+            args=(city_id, partial[:-1])#should be safe according to https://bobby-tables.com/python, I think
         else:
             #substring matching
-            sql='''SELECT * FROM waste_table WHERE city=? AND material LIKE ?'''
-            args=(city,"%"+partial+"%")
+            sql='''SELECT * FROM waste_table WHERE city_id=? AND material LIKE ?'''
+            args=(city_id,"%"+partial+"%")
         answer=con.execute(sql,args).fetchone()#return just the first match
     if answer is not None:
         result={'material':answer['material'], 'category':answer['category'], 'instructions':Markup(answer['instructions']).unescape()}
@@ -62,26 +76,27 @@ def query_db(city: str, partial:str):
 #index page functionality
 @app.route('/', methods=['GET'])
 def index():
-    #note: Calgary has no entries in the db yet
-    return render_template('home.html', cities=["Boston","Calgary","Toronto"])
+    cities=get_cities_db()
+    return render_template('home.html', cities=cities)
 
 #handle selections on index page
 @app.route('/handle_data', methods=['POST'])
 def handle_data():
-    city = request.form['city']
+    city_id = request.form['city_id']
     action= request.form['action']
     if action=="wizard":
-        return redirect(url_for('search', city=city))
+        return redirect(url_for('search', city_id=city_id))
     else:
-        return render_template('questions.html', city=city)
+        return render_template('questions.html', city_id=city_id)
 
 #handle redirect to wizard and wizard inputs for city
-@app.route('/search/<city>', methods=['POST','GET']) #GET method allows direct navigation to here
-def search(city):
-   citylist=matlist_db(city) #after each request is finished, the db connection is closed
+@app.route('/search/<city_id>', methods=['POST','GET']) #GET method allows direct navigation to here
+def search(city_id):
+   city=get_city_db(city_id)#after each request is finished, the db connection is closed
+   citylist=matlist_db(city_id) #but it stays open while in same request context
    if request.method=='POST': #form has been submitted
        partial=escape(request.form['search'])#I think flask escapes automatically
-       result=query_db(city,partial) #look into SQLalchemy for extensive query functionality
+       result=query_db(city_id,partial) #look into SQLalchemy for extensive query functionality
    else:
        result=None
    return render_template('wizard.html', city=city, citylist=citylist, result=result)
