ebf7dbffe383030914ecb17e54de8250f78c50ad
Nashmeyah <nashalrkabi@gmail.com>
2022-07-02 02:07:33 -0600

    simplified and removed unused junk

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-from website import creat_app
+from flask import Flask, redirect, url_for
 
-app = create_app()
+app = Flask(__name__)
+
+
+
+
+@app.route("/")
+def home():
+    return " <h1>Hello this is the main page!</h1>"
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}"
+
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
+
+
+
 
 
 if __name__ == '__main__':
-    app.run(debug =True)
-
-    
+    app.run()
--- website/static/templates/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-def creat_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'secretkeyissecret'
-    return app
